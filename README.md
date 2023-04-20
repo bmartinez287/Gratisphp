56c1936eea1bd0b360ef3318bbb3516d1ce1f68b
Bernardo Martinez <israelm28@gmail.com>
2023-04-19 21:37:00 -0400

    Fix: Added db dump and readme notes.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Gratisphp
 PHP sample project
+
+This project uses ddev.
+https://ddev.readthedocs.io/en/latest/users/quickstart/
+
+That said, if docker is not on a given machine it could run as native php project
--- views/dashboard/index.view.php
+++ views/dashboard/index.view.php
@@ -9,17 +9,17 @@
       <?php foreach ($cars as $car) : ?>
         <a href="/car?id=<?= $car['id'] ?>">
           <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="<?=$car['image']?>" alt="Sunset in the mountains">
+            <img class="w-full" src="<?= $car['image'] ?>" alt="Sunset in the mountains">
             <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2"> <?= htmlspecialchars($car['name']) ?></div>
               <p class="text-gray-700 text-base">
-              <?=$car['description']?>
+                <?= $car['description'] ?>
               </p>
             </div>
             <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: $<?=$car['price']?></span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Miles: <?=$car['milage']?></span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Vin: <?=$car['vin']?></span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: $<?= $car['price'] ?></span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Miles: <?= $car['milage'] ?></span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Vin: <?= $car['vin'] ?></span>
             </div>
           </div>
         </a>
